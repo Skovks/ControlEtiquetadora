89f5bc07ab932ec4fb29dd315d761171ac924a7a
Javier Alexis Marcial Flores <86272522+Skovks@users.noreply.github.com>
2022-12-21 19:13:55 -0600

    actualizacion de codigo

--- encoderRotativo_/encoderRotativo_.ino
+++ encoderRotativo_/encoderRotativo_.ino
@@ -4,7 +4,7 @@ float x1;
 float seg;
 float deltat;
 float rpm;
-float pulsos;
+float pulsos=0;
 float mul;
  
 void setup() {
@@ -12,13 +12,10 @@ void setup() {
 
   pinMode(2, INPUT_PULLUP); // internal pullup input pin 2 
   
-  //pinMode(3, INPUT_PULLUP); // internal pullup input pin 3
    //Setting up interrupt
   //A rising pulse from encodenren activated ai0(). AttachInterrupt 0 is DigitalPin nr 2 on moust Arduino.
   attachInterrupt(0, ai0, RISING);
    
-  //B rising pulse from encodenren activated ai1(). AttachInterrupt 1 is DigitalPin nr 3 on moust Arduino.
-  //attachInterrupt(1, ai1, RISING);
   }
    
   void loop() {
@@ -26,21 +23,15 @@ void setup() {
   if(x1-x0>= 1000){
     detachInterrupt(0);               // Desconectamos la interrupción para que no actué en esta parte del programa.
     deltat=x1-x0;
-    mul=pulsos*0.1;
+    mul=pulsos*100;
     rpm=abs(mul/deltat);
     seg=x1/1000;
-    Serial.print("segundos :");
-    Serial.print(seg);
-    Serial.print("   rpm: ");
-    Serial.print(rpm);
-    Serial.print("   pulsos: ");
-    Serial.println(pulsos);  
-  
+    Serial.print("rpm: ");
+    Serial.println(rpm);
     temp = pulsos;
     pulsos = 0;               // Inicializamos los pulsos.
     x0 = x1;  // Almacenamos el tiempo actual.
     attachInterrupt(0, ai0, RISING); // Reiniciamos la interrupción
-
   }
   
   }
@@ -50,13 +41,4 @@ void setup() {
   // Check pin 3 to determine the direction
   pulsos++;
   }
-   
-  /*void ai1() {
-  // ai0 is activated if DigitalPin nr 3 is going from LOW to HIGH
-  // Check with pin 2 to determine the direction
-  if(digitalRead(2)==LOW) {
-  counter--;
-  }else{
-  counter++;
-  }
-  }*/
+  
